a68b9362d1bea354d3e12f9d89fff4b6d0285545
Ingo Blechschmidt <iblech@speicherleck.de>
2025-04-09 14:05:08 +0200

    Short script for live-rebuilding

---
+++ develop.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p ttyd bubblewrap "nginxMainline.override { modules = with pkgs.nginxModules; [ brotli zstd ]; }" inotify-tools pandoc tmux "(agda.withPackages (p: [ p.standard-library p.cubical p.agda-categories p._1lab p.generics p.functional-linear-algebra ]))"
+
+./backend/boot.sh &
+./frontend/boot.sh &
+
+while :; do
+  ./frontend/build.sh
+  echo -en "\007"
+
+  inotifywait -e close_write -r Padova2025/ backend/ frontend/
+  sleep 0.2  # wait for things to settle
+done
+
+wait
