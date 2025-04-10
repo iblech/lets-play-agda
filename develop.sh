621c5596827847eb482500acc30b670a4199c069
Ingo Blechschmidt <iblech@speicherleck.de>
2025-04-10 06:47:09 +0200

    State Emacs dependency

--- develop.sh
+++ develop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -i bash -p ttyd bubblewrap "nginxMainline.override { modules = with pkgs.nginxModules; [ brotli zstd ]; }" inotify-tools pandoc tmux zip "(agda.withPackages (p: [ p.standard-library p.cubical p.agda-categories p._1lab p.generics p.functional-linear-algebra ]))"
+#! nix-shell -i bash -p ttyd bubblewrap "nginxMainline.override { modules = with pkgs.nginxModules; [ brotli zstd ]; }" inotify-tools pandoc tmux zip "(agda.withPackages (p: [ p.standard-library p.cubical p.agda-categories p._1lab p.generics p.functional-linear-algebra ]))" "(emacs-nox.pkgs.withPackages (epkgs: [ epkgs.evil epkgs.tramp-theme epkgs.ahungry-theme epkgs.color-theme-sanityinc-tomorrow epkgs.use-proxy ]))"
 
 ./backend/boot.sh &
 ./frontend/boot.sh &
